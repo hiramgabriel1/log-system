6bb9fd8b137a5178ad3f34589dd49a73a9ac7b57
itsrusty <rusty3031@gmail.com>
2023-10-05 10:41:04 -0700

    new: delete by id controller is done!

--- src/controllers/data.controllers.js
+++ src/controllers/data.controllers.js
@@ -1,29 +1,82 @@
+import taskModel from "../models/taskModel.js"
+
 class controllersData {
     constructor(name){
         this.name = name
     }
 
-    createTask(req, res){
-        const { username, years, desc } = req.body
+    // TODO: create a new task and save in database!
+    async createTask(req, res){
+        const { title, desc, required } = req.body
         try {
             const data = {
-                username,
-                years, 
-                desc
+                title: title,
+                desc: desc,
+                required: required
             }
 
-            res.json({ message: data })
+            // ? save in database
+            const isSubmitData = await taskModel.create(data)
+            
+            // ? validate if submit data is true
+            console.log(isSubmitData)
+            isSubmitData != null 
+                ? res.status(200).json({ message: "all okay!" }) 
+                : res.status(500).json({ message: "internal error" })
+
         } catch (error) {
             throw new Error(error)
         }
     }
 
-    getTasks(req, res){
+    // TODO: get all tasks!
+    async getTasks(req, res){
         try {
-            res.json({ message: "hello world" })
+            // ? try all render data
+            const dataAPI = await taskModel.find()
+            
+            dataAPI != null
+                ? res.status(200).json({ message: "find!", data: dataAPI })
+                : res.status(500).json({ message: "internal error" })
+                
         } catch (error) {
             throw new Error(error)            
         }
+    }
+
+    // TODO: delete a task by ID
+    async deleteTask(req, res){
+        try {
+            const id = req.params.id
+
+            // ? @search title in database
+            const searchByTitleInDatabase = await taskModel.findByIdAndRemove(id)
+            
+            // ? @search by id and delete
+            searchByTitleInDatabase 
+                    ? res.status(200).json({ message: "deleted!", data: searchByTitleInDatabase })
+                    : res.status(500).json({ message: "internal error", data: searchByTitleInDatabase })
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    // TODO: search task by title
+    async searchByTitleTask(req, res){
+        try {
+            
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+
+    // TODO: update a task by id
+    async updateTask(req, res){
+        try {
+            
+        } catch (error) {
+            throw new Error(error)
+        }        
     }
 }
 
