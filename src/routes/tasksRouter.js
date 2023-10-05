c3c3dceae61353b3d0757feddaf479ee7e3bec24
itsrusty <rusty3031@gmail.com>
2023-10-05 10:19:37 -0700

    new: create a new endpoints

--- src/routes/tasksRouter.js
+++ src/routes/tasksRouter.js
@@ -5,12 +5,29 @@ const controllers = new controllersData("testing");
 const path = "/api/v1";
 const routerDataCreate = Router();
 
+// TODO: endpoint to get all tasks
 routerDataCreate.get(`${path}/`, (req, res) => {
   controllers.getTasks(req, res);
 });
 
+// TODO: endpoint to get task by title
+routerDataCreate.get(`${path}/:title`, (req, res) => {
+  controllers.searchByTitleTask(req, res);
+});
+
+// TODO: endpoint to delete task
+routerDataCreate.delete(`${path}/:title`, (req, res) => {
+  controllers.deleteTask(req, res);
+});
+
+// TODO: endpoint to create a new post
 routerDataCreate.post(`${path}/create`, (req, res) => {
   controllers.createTask(req, res);
+});
+
+// TODO: update a task
+routerDataCreate.put(`${path}/:id`, (req, res) => {
+  controllers.updateTask(req, res);
 });
 
 export default routerDataCreate;
